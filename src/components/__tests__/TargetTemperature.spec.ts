c45086edb64abb661a988bdc73dca9b71b7eb90b
Marc Perrien <marc@bluelimes.dev>
2025-02-14 14:20:27 +0100

    Add tests
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>

--- src/components/__tests__/TargetTemperature.spec.ts
+++ src/components/__tests__/TargetTemperature.spec.ts
@@ -1,11 +1,31 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { mount } from "@vue/test-utils";
+import TargetTemperature from "../TargetTemperature.vue";
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe("TargetTemperature", () => {
+  it("renders properly", () => {
+    const wrapper = mount(TargetTemperature, { props: { min: 4, max: 27, target: 19 } });
+    expect(wrapper.text()).toContain("19°C");
+  });
+  it("doesn't display a target too high", () => {
+    const wrapper = mount(TargetTemperature, { props: { min: 4, max: 27, target: 29 } });
+    expect(wrapper.text()).toContain("27°C");
+  });
+  it("doesn't display a target too low", () => {
+    const wrapper = mount(TargetTemperature, { props: { min: 4, max: 27, target: 0 } });
+    expect(wrapper.text()).toContain("4°C");
+  });
+  it("gets the right angle when target===min", () => {
+    const wrapper = mount(TargetTemperature, { props: { min: 4, max: 27, target: 4 } });
+    expect(wrapper.html()).toContain("transform: rotate(-140deg)");
+  });
+  it("gets the right angle when target===max", () => {
+    const wrapper = mount(TargetTemperature, { props: { min: 4, max: 27, target: 27 } });
+    expect(wrapper.html()).toContain("transform: rotate(140deg)");
+  });
+  it("gets the right angle", () => {
+    const wrapper = mount(TargetTemperature, { props: { min: 4, max: 27, target: 19 } });
+    expect(wrapper.html()).toContain("transform: rotate(42.6");
+  });
+});

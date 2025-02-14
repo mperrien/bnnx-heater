9b64ff0c98fc9349d6087543744b9f9d508226e6
Marc Perrien <marc@bluelimes.dev>
2025-02-14 14:00:23 +0100

    Create component (#1)
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,91 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Target Temperature</h1>
+    <TargetTemperature :min="min" :max="max" :target="target" />
+    <div class="settings">
+      <form>
+        <h2>Settings</h2>
+        <div class="inputWrapper">
+          <label for="min">Min</label>
+          <input type="number" id="min" name="min" v-model="min" min="4" max="20" />
+        </div>
+        <div class="inputWrapper">
+          <label for="max">Max</label>
+          <input type="number" id="max" name="max" v-model="max" min="21" max="32" />
+        </div>
+        <div class="inputWrapper">
+          <label for="target">Target</label>
+          <input type="number" id="target" name="target" v-model="target" :min="min" :max="max" />
+        </div>
+      </form>
+    </div>
   </main>
 </template>
 
+<script setup lang="ts">
+import { ref } from "vue";
+import TargetTemperature from "@/components/TargetTemperature.vue";
+
+const min = ref<number>(4);
+const max = ref<number>(32);
+const target = ref<number>(19);
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--space-medium);
+  min-height: 100vh;
+  width: 100%;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  h1 {
+    color: var(--text-color-dim);
+    font-size: 1.6rem;
+    font-weight: 500;
+    text-transform: uppercase;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  .settings {
+    position: absolute;
+    top: var(--space-small);
+    right: var(--space-small);
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    padding: var(--space-small);
+
+    border-radius: 1.2rem;
+    box-shadow: var(--box-shadow-diffuse);
+
+    h2 {
+      margin-bottom: var(--space-small);
+
+      color: var(--text-color-dim);
+      font-size: 1.4rem;
+      font-weight: 500;
+      line-height: 1.1;
+      text-transform: uppercase;
+    }
+
+    .inputWrapper + .inputWrapper {
+      margin-top: var(--space-small);
+    }
+
+    .inputWrapper {
+      display: flex;
+
+      label {
+        padding-right: 0.6rem;
+        width: 6rem;
+
+        font-size: 1.4rem;
+      }
+
+      input {
+        width: 8rem;
+      }
+    }
   }
 }
 </style>
